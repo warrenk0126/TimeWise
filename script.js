61a6c7c7de801a8365d28cc11594f66eae0039a0
daniel gudino <dgudino459@gmail.com>
2023-08-11 23:01:35 -0700

    tailwind framework

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-const constantDivs = document.querySelectorAll('.constant div');
-        
-        constantDivs.forEach(div => {
-            div.addEventListener('click', () => {
-                div.classList.toggle('clicked');
-            });
-        });
+// Save input values to local storage
+const saveButtons = document.querySelectorAll('button[id^="saveButton"], button[id^="mealSaveButton"], button[id^="exerciseSaveButton"], button[id^="timeSaveButton"]');
+saveButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const inputId = button.previousElementSibling.id;
+    const inputValue = button.previousElementSibling.value;
+    localStorage.setItem(inputId, inputValue);
+  });
+});
+
+// Load saved input values from local storage
+const inputs = document.querySelectorAll('input');
+inputs.forEach(input => {
+  const savedValue = localStorage.getItem(input.id);
+  if (savedValue) {
+    input.value = savedValue;
+  }
+});
