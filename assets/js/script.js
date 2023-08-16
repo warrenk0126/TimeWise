5c5032a4d06b649e2325c8ae8c8dcbc4edc86b27
Jesse Navarette <j_navarette@hotmail.com>
2023-08-15 20:13:05 -0700

    update api key

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,26 @@
-
+const apiKey = 'b8e76533d49342768241dd6ce39ca8e7';
 const quotesApiKey = '6hjR+Z+iplIV9VL+StaaLg==jQnN6wcPkiNSWZDy';
 
+async function getWeatherDataByLocation(latitude, longitude) {
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+}
+
+function displayCurrentWeather(weatherData) {
+    const currentWeatherDiv = document.getElementById('current-weather');
+    currentWeatherDiv.innerHTML = `
+        <div class="weather-card">
+            <h2 class="text-3xl">${weatherData.name} (${new Date(weatherData.dt * 1000).toLocaleDateString()})</h2>
+            <img class="mx-auto" src="https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png" alt="weather icon">
+            <p><span class="font-bold">Temperature:</span> ${weatherData.main.temp}°C</p>
+            <p><span class="font-bold">Feels like:</span> ${weatherData.main.feels_like}°C</p>
+            <p><span class="font-bold">Humidity:</span> ${weatherData.main.humidity}%</p>
+            <p><span class="font-bold">Wind Speed:</span> ${weatherData.wind.speed}m/s</p>
+        </div>
+    `;
+}
 
 
 async function loadWeatherAndQuote() {
